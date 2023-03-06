6bfba0dea57d16b051c12bcc0a1fa5ce2a81a918
khaderm <khaderm@users.noreply.github.com>
2023-03-05 20:45:55 -0500

    Update tokenizedBallotVotes.ts

--- scripts/tokenizedBallotVotes.ts
+++ scripts/tokenizedBallotVotes.ts
@@ -4,14 +4,6 @@ import { Ballot__factory } from "../typechain-types";
 dotenv.config();
 
 
-function convertStringArrayToBytes32(array: string[]) {
-    const bytes32Array = [];
-    for (let index = 0; index < array.length; index++) {
-        bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
-    }
-    return bytes32Array;
-}
-
 async function main() {
     const provider = new ethers.providers.AlchemyProvider(
         "goerli",
